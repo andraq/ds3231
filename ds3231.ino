2d7aa96e6a412c79ffdb7ec202566c2915e9b0bf
andraq <andret.aquino@gmail.com>
2020-02-27 05:34:30 -0300

    versão 003 - Correção NACK

--- ds3231.ino
+++ ds3231.ino
@@ -5,15 +5,15 @@
  *  Atualização:  
   ----------------------------------------------------------------------------------*/
 
-#include <user_interface.h>
 #include "ds3231.h"
+
+#define LED 7
 
 char* charAux200 = (char *) malloc(sizeof(char) * 200);
 
 // Veriáveis RTC 
 dadosRtc relogio;
 ds3231Class rtc;
-os_timer_t tmr0;
 
 /*******************************************************************
  * SETUP
@@ -26,6 +26,10 @@ void setup() {
 
   // RTC ----------------------------------------------------------
   rtc.definePino(9, 13);
+
+  // Configura LED
+  pinMode(LED, OUTPUT);
+
   rtc.habilitaClock();
   relogio = rtc.le();
   sprintf(charAux200, "%u:%u:%u - %u/%u/%u - %u", relogio.hora, relogio.minuto, relogio.segundo, relogio.dia, relogio.mes, relogio.ano, relogio.diaSemana);
@@ -34,7 +38,6 @@ void setup() {
   Serial.println(rtc.leTemperatura());
   Serial.println();
 
-  // TESTE ATUALIZAÇÃO ---------------------------------------------
   relogio.hora = 8;
   relogio.minuto = 30;
   relogio.segundo = 0;
@@ -44,38 +47,15 @@ void setup() {
   relogio.diaSemana = 4;
   rtc.atualiza(relogio);
   
-  // INICIA TIMER --------------------------------------------------
-  os_timer_setfn(&tmr0, timer0, NULL); //Indica ao Timer qual sera sua Sub rotina.
-  os_timer_arm(&tmr0, 1000, true);
 }
 
-/*******************************************************************
- * TIMER
- ******************************************************************/
-void timer0(void*z){
- 
-  // Atualiza relogio interno
-  relogio.segundo++;
-  if(relogio.segundo >= 60){
-    relogio.segundo = 0;
-    relogio.minuto++;
-    if(relogio.minuto >= 60){
-      relogio.minuto = 0;
-      relogio.hora++;
-      if(relogio.hora >= 24){
-        relogio.hora = 0;
-        sprintf(charAux200, "%u:%u:%u - %u/%u/%u - %u", relogio.hora, relogio.minuto, relogio.segundo, relogio.dia, relogio.mes, relogio.ano, relogio.diaSemana);
-        Serial.println(charAux200);
-        relogio = rtc.le();
-      } // end if hora
-    } // end if minuto
-  } // end if segundo
-  
-} // end timer0()
-
 void loop() {
-  // Imprimi dados relogio
+  delay(400);
+  digitalWrite(LED, HIGH);
+  delay(400);
+  digitalWrite(LED, LOW);
+  relogio = rtc.le();
   sprintf(charAux200, "%u:%u:%u - %u/%u/%u - %u", relogio.hora, relogio.minuto, relogio.segundo, relogio.dia, relogio.mes, relogio.ano, relogio.diaSemana);
   Serial.println(charAux200);
-  delay(1000);
+
 }
